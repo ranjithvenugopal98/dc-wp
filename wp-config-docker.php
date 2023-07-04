c3c8ea21434677812f1743949935176050fc0815
Ranjith Venugopal <133866297+ranjithvenugopal98@users.noreply.github.com>
2023-07-04 12:30:45 +0530

    Update wp-config-docker.php

--- wp-config-docker.php
+++ wp-config-docker.php
@@ -28,7 +28,7 @@ if (!function_exists('getenv_docker')) {
 	// https://github.com/docker-library/wordpress/issues/588 (WP-CLI will load this file 2x)
 	function getenv_docker($env, $default) {
 		if ($fileEnv = getenv($env . '_FILE')) {
-			return rtrim(file_get_contents($fileEnv), "\r\n");
+			return rtrim(file_get_contents($fileEnv), "\r\n"); hello
 		}
 		else if (($val = getenv($env)) !== false) {
 			return $val;
